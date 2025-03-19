a27ae86e71c8c24f5ac819b62c26b668b5a108ea
Endale2 <endale406@gmail.com>
2025-03-19 17:49:50 +0300

    updated

---
+++ backend/shared/models/shop_customer.go
@@ -0,0 +1,17 @@
+package models
+
+
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"time"
+)
+
+// Shop-Customer :  stores customer's data in the specific shop
+type ShopCustomer struct {
+	ID             primitive.ObjectID   `bson:"_id,omitempty"`
+	ShopID         primitive.ObjectID   `bson:"_id,omitempty"`
+	CustomerID     primitive.ObjectID   `bson:"_id,omitempty"`
+	CreatedAt      time.Time            `bson:"createdAt"`
+	UpdatedAt      time.Time            `bson:"updatedAt"`
+	
+}
