657cc89bc7b12ab63318b04fdf7727b9c0a9a1af
Endale2 <endale406@gmail.com>
2025-05-12 17:23:04 +0300

    updated

--- backend/shared/models/customer.go
+++ backend/shared/models/customer.go
@@ -1,19 +1,23 @@
 package models
 
-
 import (
-	"go.mongodb.org/mongo-driver/bson/primitive"
-	"time"
+    "time"
+
+    "go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 
-
-// Shop-Customer :  stores customer's data in the specific shop
 type Customer struct {
-	ID             primitive.ObjectID   `bson:"_id,omitempty"`
-	ShopID         primitive.ObjectID   `bson:"_id,omitempty"`
-	CustomerID     primitive.ObjectID   `bson:"_id,omitempty"`
-	CreatedAt      time.Time            `bson:"createdAt"`
-	UpdatedAt      time.Time            `bson:"updatedAt"`
-	
+    ID         primitive.ObjectID `bson:"_id,omitempty" json:"id"`
+    FirstName  string             `bson:"firstName"       json:"firstName"`
+    LastName   string             `bson:"lastName"        json:"lastName"`
+    Email      string             `bson:"email"           json:"email"`
+    Phone      string             `bson:"phone"           json:"phone"`
+    Address    string             `bson:"address"         json:"address"`
+    City       string             `bson:"city"            json:"city"`
+    State      string             `bson:"state"           json:"state"`
+    PostalCode string             `bson:"postalCode"      json:"postalCode"`
+    Country    string             `bson:"country"         json:"country"`
+    CreatedAt  time.Time          `bson:"createdAt"       json:"createdAt"`
+    UpdatedAt  time.Time          `bson:"updatedAt"       json:"updatedAt"`
 }
