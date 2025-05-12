523542ae01e32906f20bf523678777dbe595bada
Endale2 <endale406@gmail.com>
2025-05-12 17:13:29 +0300

    updated

--- adminPanel/src/pages/admin/stores/StoresList.vue
+++ adminPanel/src/pages/admin/stores/StoresList.vue
@@ -1,22 +1,23 @@
 <template>
-    <div>
-      <h2 class="text-2xl mb-4">Stores</h2>
-      <ul>
-        <li v-for="s in stores" :key="s.id">
-          <router-link :to="`/stores/${s.id}`">{{ s.name || s.id }}</router-link>
-        </li>
-      </ul>
-    </div>
-  </template>
-  
-  <script>
-  import axios from 'axios';
-  export default {
-    name: 'StoresList',
-    data() { return { stores: [] }; },
-    async created() {
-      const res = await axios.get('/admin/stores', { withCredentials: true });
-      this.stores = res.data;
-    }
-  };
-  </script>
+  <div>
+    <h2 class="text-2xl mb-4">Stores</h2>
+    <ul>
+      <li v-for="s in stores" :key="s.ID">
+        <!-- use uppercase fields -->
+        <router-link :to="`/stores/${s.ID}`">{{ s.Name }}</router-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+  name: 'StoresList',
+  data() { return { stores: [] }; },
+  async created() {
+    const res = await axios.get('admin/stores', { withCredentials: true });
+    this.stores = res.data;
+  }
+};
+</script>
