a1635c3541a817f6c18e9588f25ed9a9008a1eb3
Endale2 <endale406@gmail.com>
2025-05-13 20:40:06 +0300

    updated

--- adminPanel/src/services/auth.js
+++ adminPanel/src/services/auth.js
@@ -1,5 +1,49 @@
 import axios from 'axios';
 
+let isRefreshing = false;
+let refreshSubscribers = [];
+
+function onRrefreshed(token) {
+  refreshSubscribers.forEach((callback) => callback(token));
+  refreshSubscribers = [];
+}
+
+function addRefreshSubscriber(callback) {
+  refreshSubscribers.push(callback);
+}
+
+// Axios response interceptor for token refresh
+axios.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+
+    if (error.response && error.response.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+
+      if (!isRefreshing) {
+        isRefreshing = true;
+        try {
+          const res = await axios.post('/auth/admin/refresh');
+          isRefreshing = false;
+          onRrefreshed(); // No token to pass since you're using http-only cookies
+        } catch (refreshError) {
+          isRefreshing = false;
+          return Promise.reject(refreshError);
+        }
+      }
+
+      return new Promise((resolve) => {
+        addRefreshSubscriber(() => {
+          resolve(axios(originalRequest)); // retry original request
+        });
+      });
+    }
+
+    return Promise.reject(error);
+  }
+);
+
 export default {
   user: null,
 
