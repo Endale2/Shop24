59875b26563867f0cc6d311d320ff2a30811dccd
Endale2 <endale406@gmail.com>
2025-08-08 17:13:10 +0300

    updated

--- sellersDashboard/src/services/api.js
+++ sellersDashboard/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://api.shop24.sbs',
+  baseURL: 'http://api.shop24.sbs',
   withCredentials: true,
 })
 
